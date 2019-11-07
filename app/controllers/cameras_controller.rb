6bb725ff9f8bd3f8d0fb0e193b16f7b565ebc431
Cody <lawdermiltcody@gmail.com>
2019-11-07 15:51:37 -0600

    can add 1 photo per user

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,8 +1,9 @@
 class CamerasController < ApplicationController
+    skip_before_action :authorized, only: [:index]
 
     def index
         cameras = Camera.all 
-        render json: cameras
+        render json: cameras.as_json(include: :users)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+
   has_one_attached :image
   has_secure_password
   validates :username, uniqueness: {case_sensitive: false}
   has_many :cameras
   has_many :albums
   has_many :friends
+
+
 end
